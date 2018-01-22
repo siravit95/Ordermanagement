cf05bb3d92d2df029ed8bf58377a24b4ee00cda7
DESKTOP-QO3R7OT\siravit <siravit.ngamjee@gmail.com>
2018-01-23 00:52:40 +0700

    Create routes and contorller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    public function index(){
+
+        return view('pages.product.index'); 
+    }
+
+    public function create(){
+
+        return view('pages.product.create'); 
+    }
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('management/product', 'ProductController@index');
+
+Route::get('management/product/create', 'ProductController@create');
