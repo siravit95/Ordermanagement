5732b02b4319a7e392559b1ea3e91ed7b2e3ee40
DESKTOP-QO3R7OT\siravit <siravit.ngamjee@gmail.com>
2018-01-24 00:43:51 +0700

    Create method store

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
     //
-    public function index(){
+
+    public function index()
+    {
 
         return view('pages.product.index'); 
     }
 
-    public function create(){
+    public function create()
+    {
 
         return view('pages.product.create'); 
+    }
+
+    public function store(Request $request){
+
+
+    //dd($request->all());
+
+    $input = $request->all();
+    Product::create($input);
+     //   $name = $request->input('name');
+
     }
 
 
