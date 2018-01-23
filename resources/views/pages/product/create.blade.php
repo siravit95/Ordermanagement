ae2479b49f1da1ea4200c7977502fafb11940ec1
DESKTOP-QO3R7OT\siravit <siravit.ngamjee@gmail.com>
2018-01-24 00:46:37 +0700

    Create form view of create.blade.php of product

--- resources/views/pages/product/create.blade.php
+++ resources/views/pages/product/create.blade.php
@@ -1,2 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
 <h1>เพิ่มสินค้า</h1>
+
+<hr>
+  
+<br>
+{!! Form::open(['url' => 'management/product']) !!}
+
+{!!Form::label('name','Name:')  !!}
+    
+{!!Form::text('name',null,["maxlength"=>50])!!} <br><br>
+
+{!!Form::label('number','Price:')  !!}
+
+{!!Form::number('price',null,["min"=>1,"max"=>1000])!!}  <br><br>
+
+{!!Form::label('Detail','Detail:')  !!}  <br><br>
+
+{!!Form::textarea('detail',"รายละเอียด",["cols"=>50,"rows"=>20])!!} <br><br>
+
+{!!Form::label('Quantity','Quantity:')  !!} 
+
+{!!Form::text('qty',null,["min"=>1,"max"=>100])!!} 
+
+<br><br>
+
+{!!Form::submit("ตกลง")!!}
+
+{!! Form::close() !!}
+
+
+</body>
+</html>
+
+
+
+
+
 
