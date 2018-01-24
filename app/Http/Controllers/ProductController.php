7307169eab4192bdb435bc07bd06a9d028e8e769
DESKTOP-QO3R7OT\siravit <siravit.ngamjee@gmail.com>
2018-01-24 21:49:35 +0700

    Edit and create method productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,10 +9,28 @@ class ProductController extends Controller
 {
     //
 
+    private $productModel;
+
+    public function __construct(Product $product ){
+
+        $this->productModel = $product;
+    
+    }
+
     public function index()
     {
+        $data = $this->productModel->get();
 
-        return view('pages.product.index'); 
+        return view('pages.product.index',compact('data')); 
+    }
+
+
+    public function show($id)
+    {
+        $showdata = $this->productModel->find($id);
+        return view('pages.product.show',compact('showdata'));
+
+
     }
 
     public function create()
@@ -31,6 +49,30 @@ class ProductController extends Controller
      //   $name = $request->input('name');
 
     }
+
+    public function edit($id)
+    {
+        $editdata = $this->productModel->find($id);
+        return view('pages.product.edit',compact('editdata'));
+
+        
+    }
+
+    public function update($id,Request $request )
+    {
+        $this->productModel->findOrFail($id)->update($request->all());
+        return redirect('management/product');
+        
+    }
+
+    public function destroy($id){
+
+        $this->productModel->findOrFail($id)->delete();
+        return redirect('management/product');
+
+    }
+
+
 
 
 }
