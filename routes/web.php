95bdc1d8814a3fb896014af5ad6e567f92648946
DESKTOP-QO3R7OT\siravit <siravit.ngamjee@gmail.com>
2018-01-24 21:54:01 +0700

    Create route

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,12 @@ Route::get('management/product/create', 'ProductController@create');
 
 Route::post('management/product', 'ProductController@store');
 
+Route::get('management/product/{id}', 'ProductController@show');
 
+Route::get('management/product/{id}/edit', 'ProductController@edit');
+
+Route::patch('management/product/{id}', 'ProductController@update');
+
+Route::delete('management/product/{id}', 'ProductController@destroy');
+
+Route::resource('management/user','UserController');
